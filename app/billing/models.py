5166bbc744599fd831606cba90f2a285314f2029
Artem Innokentiev <artinnok@mail.ru>
2020-07-13 17:29:53 +0300

    Small fix

--- app/billing/models.py
+++ app/billing/models.py
@@ -8,5 +8,6 @@ class Card(Common):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="card_list"
     )
+
     stripe_card_id = models.TextField()
     is_active = models.BooleanField(default=False)
