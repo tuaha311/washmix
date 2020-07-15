0f2d9fdc9b8dbbdc542f22c34d096217d5470194
Artem Innokentiev <artinnok@mail.ru>
2020-07-15 12:18:22 +0300

    Change docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 sleep 5
 
-python manage.py migrate --settings settings.production
-python manage.py collectstatic --settings settings.production --noinput
+python manage.py migrate --settings settings.dev
+python manage.py collectstatic --settings settings.dev --noinput
 
 gunicorn --bind 0.0.0.0:8000 --workers 2 settings.wsgi:application
