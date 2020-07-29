6ee7090b7ba3e4bf4d6fcbcee762efb8df1b2617
Artem Innokentiev <artinnok@mail.ru>
2020-07-29 14:19:10 +0300

    Remove legacy API

--- app/api/legacy/views/auth.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.views import PasswordResetView
-
-from api.legacy.forms import PasswordResetFormCustom
-
-
-class PasswordResetViewCustom(PasswordResetView):
-    form_class = PasswordResetFormCustom
