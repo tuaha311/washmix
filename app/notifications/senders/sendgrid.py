327500992de1fbdc17e1fce1c08bda1c108e1878
Artem Innokentiev <artinnok@mail.ru>
2020-08-05 16:24:24 +0300

    Added integration with sendgrid

---
+++ app/notifications/senders/sendgrid.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.template.loader import render_to_string
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import HtmlContent, Mail, To
+
+from notifications.senders.base import Sender
+
+
+class SendGridSender(Sender):
+    def __init__(self):
+        self._client = SendGridAPIClient(api_key=settings.SENDGRID_API_KEY).client
+
+    def send(self, recipient_list: list, event: str, context: dict = None):
+        event_info = settings.EMAIL_EVENT_INFO[event]
+
+        subject = event_info["subject"]
+        template_name = event_info["template_name"]
+        from_email = event_info.get("from_email", settings.SENDGRID_FROM_EMAIL)
+
+        recipient_list = [To(item) for item in recipient_list]
+        html_content = render_to_string(template_name, context=context)
+
+        mail = Mail(
+            from_email=from_email,
+            to_emails=recipient_list,
+            subject=subject,
+            html_content=HtmlContent(html_content),
+        )
+        request_body = mail.get()
+
+        self._client.mail.send.post(request_body=request_body)
