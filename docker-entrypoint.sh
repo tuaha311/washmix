e08a46e93e59f516dab5ec2a06c90bd34bbbda88
Artem Innokentiev <artinnok@mail.ru>
2020-08-05 20:39:05 +0300

    Added default fill script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,7 @@ sleep 5
 
 python manage.py migrate --settings settings.dev
 python manage.py collectstatic --settings settings.dev --noinput
+python manage.py fill_packages --settings settings.dev
+python manage.py fill_cities --settings settings.dev --noinput
 
 gunicorn --bind 0.0.0.0:8000 --workers 2 settings.wsgi:application
