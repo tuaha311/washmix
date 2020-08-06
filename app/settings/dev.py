a5acb5a65d39aa15244cbb31e00b29f230b5979a
Artem Innokentiev <artinnok@mail.ru>
2020-08-06 13:50:59 +0300

    Added sentry-sdk

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -1,6 +1,9 @@
 import datetime
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from settings.base import *
 
 DEBUG = True
@@ -10,3 +13,12 @@ INSTALLED_APPS += []
 
 
 MIDDLEWARE += []
+
+
+sentry_sdk.init(
+    dsn="https://cf2158b8940747c78607487f5a2ef3ca@o430742.ingest.sentry.io/5379821",
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
