23d26d5c94a9b8310c353d9936090d0e310ebd50
Artem Innokentiev <artinnok@mail.ru>
2020-08-06 14:41:35 +0300

    Fix signup error

--- app/users/managers/client.py
+++ app/users/managers/client.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.db import models, transaction
+from django.db import models, transaction, IntegrityError
 
 from core.models import Phone
 
@@ -8,13 +8,21 @@ User = get_user_model()
 
 class ClientManager(models.Manager):
     def create_client(self, email, password, number):
-        with transaction.atomic():
-            user = User.objects.create_user(email, password, is_active=True)
+        # when we have multiple workers, we can face with
+        # situation when 2 simultaneous requests came to 2 different
+        # gunicorn workers and pass validation checks and trying to
+        # write records into database
+        try:
+            with transaction.atomic():
+                user = User.objects.create_user(email, password, is_active=True)
 
-            client = self.create(user=user)
-            phone = Phone.objects.create(client=client, number=number,)
+                client = self.create(user=user)
+                phone = Phone.objects.create(client=client, number=number,)
 
-            client.main_phone = phone
-            client.save()
+                client.main_phone = phone
+                client.save()
+        except IntegrityError:
+            user = User.objects.get(email=email)
+            client = user.client
 
         return client
