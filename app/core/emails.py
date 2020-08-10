ddd199d3f51caf64b29ad0ae16c12d126b11a33b
Artem Innokentiev <artinnok@mail.ru>
2020-08-10 16:16:06 +0300

    Added SendGridEmail with reimplemented .send method

---
+++ app/core/emails.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+
+from djoser import email
+from templated_mail.mail import BaseEmailMessage
+
+from notifications.senders.sendgrid import SendGridSender
+
+
+class SendGridEmail(BaseEmailMessage):
+    event = "-"
+
+    def send(self, to, *args, **kwargs):
+        self.render()
+
+        sender = SendGridSender()
+
+        email_info = self._get_email_info()
+        sender.raw_send(
+            recipient_list=to, html_content=self.html, **email_info,
+        )
+
+        return len(to)
+
+    def _get_email_info(self):
+        event_info = settings.EMAIL_EVENT_INFO[self.event]
+        subject = event_info["subject"]
+        from_email = event_info.get("from_email", settings.SENDGRID_FROM_EMAIL)
+
+        return {
+            "subject": subject,
+            "from_email": from_email,
+        }
+
+
+class PasswordResetEmail(SendGridEmail, email.PasswordResetEmail):
+    event = settings.FORGOT_PASSWORD
