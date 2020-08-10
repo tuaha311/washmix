e51d6e474fa281c91bc19f7b487d6caef7fa037c
Artem Innokentiev <artinnok@mail.ru>
2020-08-10 16:58:55 +0300

    Reset URL fix

--- app/settings/base.py
+++ app/settings/base.py
@@ -183,9 +183,9 @@ SIMPLE_JWT = {
 ##########
 
 DJOSER = {
-    "PASSWORD_RESET_CONFIRM_URL": "/password/reset/confirm/{uid}/{token}",
-    "USERNAME_RESET_CONFIRM_URL": "/username/reset/confirm/{uid}/{token}",
-    "ACTIVATION_URL": "/activate/{uid}/{token}",
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+    "USERNAME_RESET_CONFIRM_URL": "username/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "activate/{uid}/{token}",
     "SEND_ACTIVATION_EMAIL": True,
     "SEND_CONFIRMATION_EMAIL": True,
     "USER_CREATE_PASSWORD_RETYPE": False,
