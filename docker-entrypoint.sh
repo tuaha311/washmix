d56b49b19d1500458898577200b4a8caf975d498
Artem Innokentiev <artinnok@mail.ru>
2020-09-03 14:23:54 +0900

    Added --noinput flag

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 sleep 5
 
-python manage.py migrate --settings settings.staging
-python manage.py collectstatic --settings settings.staging
+python manage.py migrate --settings settings.staging --noinput
+python manage.py collectstatic --settings settings.staging --noinput
 python manage.py fill --settings settings.staging
 
 gunicorn --bind 0.0.0.0:8000 --workers 2 settings.wsgi:application
