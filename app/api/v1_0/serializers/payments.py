f9ab186978e76efaf9644c2c1bfd0892b6c3c7a7
Artem Innokentiev <artinnok@mail.ru>
2020-09-04 13:13:06 +0900

    Remove /charge_payment/ endpoint + payment_method added to SetupIntentView

--- app/api/v1_0/serializers/payments.py
+++ app/api/v1_0/serializers/payments.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from billing.models import Card
 
 
 class SetupIntentSerializer(serializers.Serializer):
-    pass
-
-
-class ChargePaymentSerializer(serializers.Serializer):
-    # TODO read queryset from request.user.client
     card = serializers.PrimaryKeyRelatedField(queryset=Card.objects.all())
-    amount = serializers.DecimalField(max_digits=9, decimal_places=2)
+
+    def validate_card(self, value):
+        client = self.context["request"].user.client
+
+        get_object_or_404(client.card_list.all(), pk=value.pk)
+
+        return value
 
 
 class StripeWebhookSerializer(serializers.Serializer):
