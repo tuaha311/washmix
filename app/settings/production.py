9ebda64750c364c03d855f3f6dfd1aa702f0af2e
Artem Innokentiev <artinnok@mail.ru>
2020-09-11 14:37:46 +0900

    Split entrypoint into multiple components

--- app/settings/production.py
+++ app/settings/production.py
@@ -5,10 +5,3 @@ DEBUG = False
 PREPEND_WWW = True
 
 ALLOWED_HOSTS = ["*"]
-
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
-        "LOCATION": "127.0.0.1:11211",
-    }
-}
