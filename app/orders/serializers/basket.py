f8825fa244658fe84acd3f715da24ba973137f0e
Artem Innokentiev <artinnok@mail.ru>
2020-09-23 14:44:54 +0900

    WM-74 Added F-expression for increment and decrement

--- app/orders/serializers/basket.py
+++ app/orders/serializers/basket.py
@@ -3,12 +3,24 @@ from django.conf import settings
 from rest_framework import serializers
 
 from orders.models import Price, Quantity
+from orders.services.basket import BasketService
 
 
 class ChangeItemSerializer(serializers.Serializer):
     price = serializers.PrimaryKeyRelatedField(queryset=Price.objects.all())
     count = serializers.IntegerField()
     action = serializers.ChoiceField(choices=settings.BASKET_ACTION_CHOICES)
+
+    def validate(self, attrs):
+        client = self.context["request"].user.client
+        price = attrs["price"]
+        count = attrs["count"]
+        action = attrs["action"]
+
+        service = BasketService(client)
+        service.validate(price, count, action)
+
+        return attrs
 
 
 class ChangeItemResponseSerializer(serializers.ModelSerializer):
--- app/orders/services/basket.py
+++ app/orders/services/basket.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.db.models import F
 from django.db.transaction import atomic
+
+from rest_framework import serializers
 
 from orders.models import Basket, Price, Quantity
 from users.models import Client
@@ -8,37 +12,33 @@ class BasketService:
     def __init__(self, client: Client):
         self._client = client
 
-    @property
-    def basket(self) -> Basket:
-        basket, _ = Basket.objects.get_or_create(client=self._client, order__isnull=True,)
-        return basket
+    def validate(self, price: Price, count: int, action: str):
+        quantity = self._get_or_create_quantity(price)
 
-    def validate(self):
-        pass
-
-    def get_or_create_quantity(self, price: Price) -> Quantity:
-        quantity, _ = Quantity.objects.get_or_create(
-            basket=self.basket, price=price, defaults={"count": 0,},
-        )
-        return quantity
+        if action == settings.BASKET_REMOVE and quantity.count < count:
+            raise serializers.ValidationError(
+                detail="You can't remove more items that in basket.",
+                code="cant_perform_item_remove",
+            )
 
     def add_item(self, price: Price, count: int) -> Basket:
         with atomic():
-            quantity = self.get_or_create_quantity(price)
+            quantity = self._get_or_create_quantity(price)
 
-            # TODO использовать F-expression
-            quantity.count += count
+            quantity.count = F("count") + count
             quantity.save()
 
         return self.basket
 
     def remove_item(self, price: Price, count: int) -> Basket:
         with atomic():
-            quantity = self.get_or_create_quantity(price)
+            quantity = self._get_or_create_quantity(price)
 
-            # TODO использовать F-expression
-            quantity.count -= count
+            quantity.count = F("count") - count
             quantity.save()
+
+            if quantity.count == 0:
+                quantity.delete()
 
         return self.basket
 
@@ -48,3 +48,14 @@ class BasketService:
 
     def checkout(self):
         pass
+
+    @property
+    def basket(self) -> Basket:
+        basket, _ = Basket.objects.get_or_create(client=self._client, order__isnull=True,)
+        return basket
+
+    def _get_or_create_quantity(self, price: Price) -> Quantity:
+        quantity, _ = Quantity.objects.get_or_create(
+            basket=self.basket, price=price, defaults={"count": 0,},
+        )
+        return quantity
