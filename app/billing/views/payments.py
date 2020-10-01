55155a6f515255a3ded21639a78bd97690cc6b9c
Artem Innokentiev <artinnok@mail.ru>
2020-10-01 16:25:40 +0900

    Disable IP check on Stripe

--- app/billing/views/payments.py
+++ app/billing/views/payments.py
@@ -38,7 +38,7 @@ class CreateIntentView(GenericAPIView):
 
 class StripeWebhookView(GenericAPIView):
     permission_classes = [AllowAny]
-    enable_ip_check = True
+    enable_ip_check = False
 
     def post(self, request: Request, *args, **kwargs):
         # we will use Stripe SDK to check validity of event instead
@@ -51,7 +51,7 @@ class StripeWebhookView(GenericAPIView):
 
             # don't allowing other IPs excluding Stripe's IPs
             if ip_address not in settings.STRIPE_WEBHOOK_IP_WHITELIST:
-                return Response(data={"id": ip_address,}, status=403,)
+                return Response(data={"ip": ip_address,}, status=403,)
 
         if event.type in ["payment_intent.succeeded", "charge.succeeded"]:
             payment = event.data.object
