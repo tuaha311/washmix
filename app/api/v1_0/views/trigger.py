7a1c09454e64304b81f3f48406187fff5ae0daf1
Artem Innokentiev <artinnok@mail.ru>
2020-10-05 17:02:49 +0900

    Bugfix

--- app/api/v1_0/views/trigger.py
+++ app/api/v1_0/views/trigger.py
@@ -2,12 +2,12 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from core.tasks import count
+from core.tasks import worker_health
 
 
 class TriggerView(GenericAPIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        count.send(100)
+        worker_health.send(100)
         return Response({"trigger": "queued"})
