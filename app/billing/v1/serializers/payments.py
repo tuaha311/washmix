c4b7e729eb95c88ca5407549a36865b4b7158110
Artem Innokentiev <artinnok@mail.ru>
2020-10-12 14:05:03 +0900

    WM-203 Added handling for PAYC among with is_save_card=False

--- app/billing/v1/serializers/payments.py
+++ app/billing/v1/serializers/payments.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from rest_framework import serializers
 
 from api.fields import InvoiceField
@@ -6,6 +8,18 @@ from api.fields import InvoiceField
 class CreateIntentSerializer(serializers.Serializer):
     is_save_card = serializers.BooleanField()
     invoice = InvoiceField()
+
+    def validate(self, attrs):
+        invoice = attrs["invoice"]
+        is_save_card = attrs["is_save_card"]
+
+        if invoice.subscription.name == settings.PAYC and is_save_card == False:
+            raise serializers.ValidationError(
+                detail="For PAYC package you should save payment card.",
+                code="card_save_required_for_payc",
+            )
+
+        return attrs
 
 
 class CreateIntentResponseSerializer(serializers.Serializer):
