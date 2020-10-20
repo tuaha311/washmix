3e364e65b41d10f4ae15a7b89d8f056c397dc022
Artem Innokentiev <artinnok@mail.ru>
2020-10-20 11:17:46 +0900

    WM-183 Added Delivery price map

--- app/deliveries/containers.py
+++ app/deliveries/containers.py
@@ -2,15 +2,53 @@ from django.conf import settings
 
 from core.utils import get_dollars
 from deliveries.models import Delivery
+from orders.models import Basket
 from subscriptions.choices import Package
 from subscriptions.models import Subscription
 from users.models import Client
 
+PAYC_FREE_DELIVERY_THRESHOLD = 4900
+GOLD_PLATINUM_FREE_DELIVERY_THRESHOLD = 3900
 
-class DeliveryDiscountService:
-    def __init__(self, client: Client, subscription: Subscription, delivery: Delivery):
+
+class DeliveryContainer:
+    """
+    Reference to Delivery Fees - https://washmix.evrone.app/terms-of-use
+    """
+
+    price_map = {
+        (Package.PAYC,): {
+            "free_threshold": PAYC_FREE_DELIVERY_THRESHOLD,
+            "price_list": [
+                {"min": 0, "max": 3000, "price": 1990,},
+                {"min": 3100, "max": 4800, "price": 990,},
+            ],
+        },
+        (Package.GOLD, Package.PLATINUM,): {
+            "free_threshold": GOLD_PLATINUM_FREE_DELIVERY_THRESHOLD,
+            "price_list": [
+                {"min": 0, "max": 2500, "price": 1498,},
+                {"min": 2600, "max": 3800, "price": 990,},
+            ],
+        },
+    }
+
+    def __init__(
+        self, client: Client, subscription: Subscription, delivery: Delivery, basket: Basket
+    ):
         self._client = client
         self._subscription = subscription
+        self._delivery = delivery
+        self._basket = basket
+
+    def __getattr__(self, item):
+        """
+        This method invoked only when we can't find attribute name in itself.
+        Method works as a fallback.
+        """
+
+        delivery = self._delivery
+        return getattr(delivery, item)
 
     @property
     def amount(self) -> int:
