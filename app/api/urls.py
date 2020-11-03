f28d25c8eb918508a1989925abdee286b7c22c7f
Artem Innokentiev <artinnok@mail.ru>
2020-11-03 18:37:02 +0900

    Remove v1.0 namespace

--- app/api/urls.py
+++ app/api/urls.py
@@ -4,7 +4,6 @@ from api import views
 
 urlpatterns = [
     path("health/", views.HealthView.as_view(), name="health"),
-    path("v1.0/", include("api.client.urls", namespace="v1.0")),
     path("client/", include("api.client.urls", namespace="client")),
     path("pos/", include("api.pos.urls", namespace="pos")),
 ]
