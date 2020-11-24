57687adee295e43dff9951897bef5ad3cb9d7d11
Artem Innokentiev <artinnok@mail.ru>
2020-11-24 16:07:18 +0900

    WM-281 Added base view for admin

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 
 from core.admin import DefaultAdmin
 from orders.models import Item, Order, Price, Service
+from other.media import Media
 
 
 class OrderAdmin(DefaultAdmin):
-    class Media:
-        js = ("https://washmix.evrone.app/admin-static/static/js/main.js",)
-        css = {"all": ("https://washmix.evrone.app/admin-static/static/css/main.css",)}
+    Media = Media
 
 
 models = [
--- app/other/site.py
+++ app/other/site.py
@@ -1,14 +1,20 @@
 from functools import update_wrapper
 
+from django import forms
 from django.contrib.admin import AdminSite
 from django.urls import path
 from django.views.generic.base import TemplateResponse
 
+from other.media import Media
 
-class WashmixAdminSite(AdminSite):
+
+# metaclass definition copied from django.contrib.admin.options.BaseModelAdmin
+class WashmixAdminSite(AdminSite, metaclass=forms.MediaDefiningClass):
     site_header = "Washmix"
     site_title = "washmix.com"
     index_title = "Washmix Admin Panel"
+
+    Media = Media
 
     def _build_app_dict(self, request, label=None):
         app_dict = super()._build_app_dict(request, label)
@@ -18,7 +24,7 @@ class WashmixAdminSite(AdminSite):
     def get_urls(self):
         urls = super().get_urls()
 
-        # start of copied part from AdminSite.get_urls
+        # start of copied part from django.contrib.admin.sites.AdminSite#get_urls
         def wrap(view, cacheable=False):
             def wrapper(*args, **kwargs):
                 return self.admin_view(view, cacheable)(*args, **kwargs)
@@ -29,18 +35,20 @@ class WashmixAdminSite(AdminSite):
         # end of copied part
 
         custom_urls = [
-            path("pos", wrap(self.pos), name="pos"),
+            path("pos/", wrap(self.pos), name="pos"),
         ]
 
         return urls + custom_urls
 
     def pos(self, request, extra_context=None):
-        # start of copied part from AdminSite views
+        # start of copied part from django.contrib.admin.sites.AdminSite views
         request.current_app = self.name
 
         context = {
             **self.each_context(request),
             "title": self.index_title,
+            # injected by metaclass
+            "media": self.media,
             **(extra_context or {}),
         }
         # end of copied part
