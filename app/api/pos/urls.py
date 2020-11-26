ce9bb0b6bdb6f5a32f01dae48260bd579e058158
Artem Innokentiev <artinnok@mail.ru>
2020-11-26 23:13:28 +0900

    Added /pos/coupons/ endpoint

--- app/api/pos/urls.py
+++ app/api/pos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from api.pos.views import ItemListView
+from api.pos.views import CouponListView, ItemListView
 from orders.urls.pos import basket_urls, order_urls
 
 app_name = "pos"
@@ -10,4 +10,5 @@ urlpatterns = [
     path("orders/", include(order_urls)),
     path("requests/", include("deliveries.urls.pos")),
     path("items/", ItemListView.as_view(), name="item-list"),
+    path("coupons/", CouponListView.as_view(), name="coupon-list"),
 ]
--- app/api/pos/views.py
+++ app/api/pos/views.py
@@ -1,11 +1,18 @@
 from rest_framework.generics import ListAPIView
 
 from api.permissions import default_pos_permissions
-from api.pos.serializers import ItemSerializer
+from api.pos.serializers import CouponSerializer, ItemSerializer
+from billing.models import Coupon
 from orders.models import Item
 
 
 class ItemListView(ListAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.order_by("id")
+    permission_classes = default_pos_permissions
+
+
+class CouponListView(ListAPIView):
+    serializer_class = CouponSerializer
+    queryset = Coupon.objects.all()
     permission_classes = default_pos_permissions
