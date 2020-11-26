d34c907e5080e075f26c4974663dc350f05f39c6
Artem Innokentiev <artinnok@mail.ru>
2020-11-26 23:24:52 +0900

    Added SessionBasedAuthentication for admin

--- app/api/pos/views.py
+++ app/api/pos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 
+from api.authentication import default_pos_authentication
 from api.permissions import default_pos_permissions
 from api.pos.serializers import CouponSerializer, ItemSerializer
 from billing.models import Coupon
@@ -9,10 +10,12 @@ from orders.models import Item
 class ItemListView(ListAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.order_by("id")
+    authentication_classes = default_pos_authentication
     permission_classes = default_pos_permissions
 
 
 class CouponListView(ListAPIView):
     serializer_class = CouponSerializer
     queryset = Coupon.objects.all()
+    authentication_classes = default_pos_authentication
     permission_classes = default_pos_permissions
--- app/orders/api/pos/views/checkout.py
+++ app/orders/api/pos/views/checkout.py
@@ -2,6 +2,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from api.authentication import default_pos_authentication
 from api.permissions import default_pos_permissions
 from orders.api.pos.serializers.orders import OrderCheckoutSerializer, OrderSerializer
 from orders.services.order import OrderService
@@ -10,6 +11,7 @@ from orders.services.order import OrderService
 class OrderCheckoutView(GenericAPIView):
     serializer_class = OrderCheckoutSerializer
     response_serializer_class = OrderSerializer
+    authentication_classes = default_pos_authentication
     permission_classes = default_pos_permissions
 
     def post(self, drf_request: Request, *args, **kwargs):
