93f0cbaf3062787a1ac4b90cf5a250218e0193bc
Artem Innokentiev <artinnok@mail.ru>
2020-11-30 16:16:14 +0900

    WM-296 Added new amount calculation for BasketContainer

--- app/orders/containers/basket.py
+++ app/orders/containers/basket.py
@@ -22,10 +22,23 @@ class BasketContainer(BaseAmountContainer):
 
     @property
     def amount(self) -> int:
-        return self._calculate_sum("amount")
+        # first of all, we are aggregating default item prices
+        items_amount = self._calculate_sum("amount")
+
+        # then we are adding to them extra items amount
+        extra_items = self.extra_items
+        extra_items_amount = sum([item.amount for item in extra_items])
+
+        # let's calculate total
+        total = items_amount + extra_items_amount
+
+        return total
 
     @property
     def discount(self) -> int:
+        # here we are just calculating discount of default items
+        # without worrying about extra items, because extra items
+        # doesn't have a discount
         return self._calculate_sum("discount")
 
     @property
