865ecf5313f7eb1774284741a894649c6fb8db38
Artem Innokentiev <artinnok@mail.ru>
2020-12-03 15:59:10 +0900

    Bugfix apply_coupon

---
+++ app/orders/api/pos/serializers/coupons.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+from rest_framework import serializers
+
+from api.fields import POSOrderField
+from billing.models import Coupon
+
+
+class POSApplyCouponSerializer(serializers.Serializer):
+    coupon = serializers.SlugRelatedField(slug_field="code", queryset=Coupon.objects.all())
+    order = POSOrderField()
+
+    def validate_order(self, value):
+        order = value
+        subscription = order.subscription
+
+        if subscription and subscription.name == settings.PAYC:
+            raise serializers.ValidationError(
+                detail="You cannot apply coupon to PAYC.",
+                code="no_coupon_for_payc",
+            )
+
+        return value
--- app/orders/api/pos/views/coupons.py
+++ app/orders/api/pos/views/coupons.py
@@ -1,10 +1,12 @@
 from api.authentication import default_pos_authentication
 from api.permissions import default_pos_permissions
 from orders.api.client.views.coupons import OrderApplyCouponView
+from orders.api.pos.serializers.coupons import POSApplyCouponSerializer
 from orders.models import Order
 
 
 class POSOrderApplyCouponView(OrderApplyCouponView):
+    serializer_class = POSApplyCouponSerializer
     authentication_classes = default_pos_authentication
     permission_classes = default_pos_permissions
 
