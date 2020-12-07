a7891f56db981e5f3706d5fb7cf4ce41c3c11485
Artem Innokentiev <artinnok@mail.ru>
2020-12-07 16:19:10 +0900

    WM-304 Added login

--- app/api/driver/urls.py
+++ app/api/driver/urls.py
@@ -1,5 +1,8 @@
+from django.urls import include, path
+
 from rest_framework.routers import SimpleRouter
 
+from api.client.views.auth import LoginView
 from deliveries.api.driver.views import DeliveryViewSet
 
 router = SimpleRouter(trailing_slash=True)
@@ -8,4 +11,15 @@ router.register("deliveries", DeliveryViewSet, basename="deliveries")
 app_name = "driver"
 
 
-urlpatterns = [*router.urls]
+auth_urls = (
+    [
+        path("login/", LoginView.as_view(), name="obtain"),
+    ],
+    "auth",
+)
+
+
+urlpatterns = [
+    *router.urls,
+    path("auth/", include(auth_urls)),
+]
