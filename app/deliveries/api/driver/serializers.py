47e85f55e8e4d1eabb14511008516f5869ebe255
Artem Innokentiev <artinnok@mail.ru>
2020-12-09 14:22:26 +0900

    WM-313 Add more info about address in /deliveries

--- app/deliveries/api/driver/serializers.py
+++ app/deliveries/api/driver/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
 
 from deliveries.models import Delivery
+from locations.api.serializers.addresses import AddressSerializer
 
 
 class DeliverySerializer(serializers.ModelSerializer):
+    address = AddressSerializer(read_only=True)
+
     class Meta:
         model = Delivery
-        exclude = [
-            "created",
-            "changed",
-            "employee",
+        fields = [
+            "id",
+            "kind",
+            "status",
+            "date",
+            "start",
+            "end",
+            "address",
+            "comment",
+            "is_rush",
         ]
