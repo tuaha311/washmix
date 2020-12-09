128a60ec504aab0aa544c2e46ada78cb8763cfb2
Artem Innokentiev <artinnok@mail.ru>
2020-12-09 15:32:06 +0900

    WM-315 Added client info

--- app/deliveries/api/driver/serializers.py
+++ app/deliveries/api/driver/serializers.py
@@ -2,10 +2,25 @@ from rest_framework import serializers
 
 from deliveries.models import Delivery
 from locations.api.serializers.addresses import AddressSerializer
+from users.models import Client
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    main_phone = serializers.SlugRelatedField(slug_field="number", read_only=True)
+
+    class Meta:
+        model = Client
+        fields = [
+            "id",
+            "main_phone",
+            "first_name",
+            "last_name",
+        ]
 
 
 class DeliverySerializer(serializers.ModelSerializer):
     address = AddressSerializer(read_only=True)
+    client = ClientSerializer(read_only=True)
 
     class Meta:
         model = Delivery
@@ -20,4 +35,5 @@ class DeliverySerializer(serializers.ModelSerializer):
             "comment",
             "is_rush",
             "priority",
+            "client",
         ]
