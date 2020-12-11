3eda419bfcac45f0b754b46ee7e232002ca94827
Artem Innokentiev <artinnok@mail.ru>
2020-12-11 16:58:12 +0900

    Regenerate migrations

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -1,14 +1,35 @@
+from django import forms
 from django.contrib import admin
 
 from core.admin import DefaultAdmin
-from orders.models import Basket, Item, Order, Price, Service
+from orders.models import Basket, Item, Order, Price, Quantity, Service
+
+
+class QuantityInlineForm(forms.ModelForm):
+    class Meta:
+        model = Quantity
+        fields = [
+            "price",
+            "count",
+        ]
+
+
+class QuantityInlineAdmin(admin.TabularInline):
+    model = Quantity
+    form = QuantityInlineForm
+    extra = 1
+
+
+class BasketAdmin(DefaultAdmin):
+    inlines = [QuantityInlineAdmin]
+
 
 models = [
     [Order, DefaultAdmin],
     [Item, DefaultAdmin],
     [Service, DefaultAdmin],
     [Price, DefaultAdmin],
-    [Basket, DefaultAdmin],
+    [Basket, BasketAdmin],
 ]
 for item in models:
     admin.site.register(*item)
