d98b2209c51a96f755b00d2209ecc79d351278b8
Artem Innokentiev <artinnok@mail.ru>
2020-12-22 13:38:33 +0900

    WM-318 Added Group to admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin.sites import NotRegistered
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Permission
 
 from djangoql.admin import DjangoQLSearchMixin
 from social_django.models import Association, Nonce, UserSocialAuth
@@ -16,7 +16,7 @@ registered_models = [[Phone, DefaultAdmin]]
 for item in registered_models:
     admin.site.register(*item)
 
-unregistered_models = [Group, Permission, Association, Nonce, UserSocialAuth]
+unregistered_models = [Permission, Association, Nonce, UserSocialAuth]
 for item in unregistered_models:
     try:
         admin.site.unregister(item)
