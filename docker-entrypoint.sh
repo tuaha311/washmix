abde2261e933f308330c64400992c5e1aa2766c5
Igor Ivanov <igor.ivanov@evrone.com>
2021-01-10 18:57:18 +0700

    Testing deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,14 +20,19 @@ then
   periodiq -v settings.dramatiq
 
 else
-  echo "Running django application"
-  python manage.py collectstatic --settings settings.staging --noinput
-  python manage.py loaddata dump.json --settings settings.staging
   ###Added for use in Heroku
   if [ -n "$PORT" ]
   then
+    echo "Running migrations"
+    python manage.py migrate --settings settings.staging --noinput
+    echo "Running django application"
+    python manage.py collectstatic --settings settings.staging --noinput
+    python manage.py loaddata dump.json --settings settings.staging
     gunicorn --bind 0.0.0.0:${PORT} --workers 2 settings.wsgi:application
   else
+    echo "Running django application"
+    python manage.py collectstatic --settings settings.staging --noinput
+    python manage.py loaddata dump.json --settings settings.staging
     gunicorn --bind 0.0.0.0:8000 --workers 2 settings.wsgi:application
   fi
 fi
