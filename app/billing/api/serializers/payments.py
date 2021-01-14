802fce7c861b6889e708321e0aaab22d4bead0e9
Artem Innokentiev <artinnok@mail.ru>
2021-01-14 16:40:20 +0900

    WM-385 Added handling for case, when we have not order

--- app/billing/api/serializers/payments.py
+++ app/billing/api/serializers/payments.py
@@ -7,13 +7,19 @@ from api.fields import OrderField
 
 class CreateIntentSerializer(serializers.Serializer):
     is_save_card = serializers.BooleanField()
-    order = OrderField()
+    order = OrderField(default=None, required=False)
 
     def validate(self, attrs):
         order = attrs["order"]
         is_save_card = attrs["is_save_card"]
 
-        if order.subscription.name == settings.PAYC and not is_save_card:
+        if not order and not is_save_card:
+            raise serializers.ValidationError(
+                detail="To bind a new payment method you should save a card.",
+                code="card_save_required_to_bind",
+            )
+
+        if order and order.subscription.name == settings.PAYC and not is_save_card:
             raise serializers.ValidationError(
                 detail="For PAYC package you should save payment card.",
                 code="card_save_required_for_payc",
--- app/billing/services/intent.py
+++ app/billing/services/intent.py
@@ -1,10 +1,11 @@
-from typing import Union
+from typing import Optional, Union
 
 from django.db.transaction import atomic
 
 from stripe import PaymentIntent, SetupIntent
 
 from billing.choices import Purpose
+from billing.models import Invoice
 from billing.services.invoice import InvoiceService
 from billing.services.payments import PaymentService
 from orders.models import Order
@@ -14,17 +15,23 @@ class IntentService:
     def __init__(self, client):
         self._client = client
 
-    def create_intent(self, order: Order, is_save_card: bool) -> Union[PaymentIntent, SetupIntent]:
+    def create_intent(
+        self, order: Optional[Order], is_save_card: bool
+    ) -> Union[PaymentIntent, SetupIntent]:
         client = self._client
-        subscription = order.subscription
-        amount = subscription.price
+        # here we are using Invoice model like container for case
+        # when client want to bind a new card (without any order)
+        invoice = Invoice()
 
         with atomic():
-            invoice_service = InvoiceService(client)
-            invoice = invoice_service.update_or_create(order, amount, Purpose.SUBSCRIPTION)
-            InvoiceService.update_invoice(invoice, is_save_card)
+            if order:
+                subscription = order.subscription
+                amount = subscription.price
+
+                invoice_service = InvoiceService(client)
+                invoice = invoice_service.update_or_create(order, amount, Purpose.SUBSCRIPTION)
 
             payment_service = PaymentService(client, invoice)
-            intent = payment_service.create_intent()
+            intent = payment_service.create_intent(is_save_card)
 
         return intent
