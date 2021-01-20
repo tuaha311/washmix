4c1f9ac40f9f14ad5c10664b85652a579a8eb1fd
Artem Innokentiev <artinnok@mail.ru>
2021-01-20 18:17:01 +0900

    WM-364 Added basic styling for emails

--- app/settings/base.py
+++ app/settings/base.py
@@ -287,6 +287,7 @@ TWITTER_URL = "https://twitter.com/mix_wash/"
 INSTAGRAM_URL = "https://www.instagram.com/washmix/"
 FACEBOOK_URL = "https://www.facebook.com/WASHMIXOriginal/"
 PHONE_NUMBER = "4​15-993-WASH [9724]"
+LAUNDRY_ADDRESS = "4949 Market St. San Francisco CA 94132"
 
 
 #########################
