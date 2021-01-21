db37af6473265783930f6e138e14d21c8d8eeb32
Artem Innokentiev <artinnok@mail.ru>
2021-01-21 16:18:30 +0900

    WM-364 Added more styling on email

--- app/api/views.py
+++ app/api/views.py
@@ -1,8 +1,12 @@
+from django.views.generic import TemplateView
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from api.serializers import HealthSerializer
+from notifications.context import email_context
+from users.models import Client
 
 
 class HealthView(GenericAPIView):
@@ -17,3 +21,19 @@ class HealthView(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return Response({"status": "ok"})
+
+
+class RenderView(TemplateView):
+    template_name = "email/signup.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        client = Client.objects.get(pk=2)
+
+        extra_context = {
+            "washmix": email_context,
+            "client": client,
+        }
+
+        return {**context, **extra_context}
