ec512a601ef4bde6ad0d705f249d368433d90f4c
Igor Ivanov <igor.ivanov@evrone.com>
2021-01-27 13:21:51 +0700

    Disable helm atomic option for debug

--- .charts/update.sh
+++ .charts/update.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CIRCLE_SHA1="${CIRCLE_SHA1:-latest}"
 helm upgrade washmix-back $DIR/washmix-back \
      --set image.tag=${CIRCLE_SHA1} \
-#     --atomic \
      --timeout=120s \
      --install && \
 
 echo "Done"
+#     --atomic \
