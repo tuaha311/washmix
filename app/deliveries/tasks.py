82b938297c98f176edcbf718982d44e7e192e4a0
Artem Innokentiev <artinnok@mail.ru>
2021-04-19 12:15:52 +0900

    WM-522 Changed request creation

--- app/deliveries/tasks.py
+++ app/deliveries/tasks.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 # every day at 06:00
 @dramatiq.actor(
-    periodic=cron("00 06 * * *"),
+    periodic=cron("*/5 * * * *"),
     max_retries=settings.DRAMATIQ_MAX_RETRIES,
     max_age=settings.DRAMATIQ_MAX_AGE,
 )
@@ -28,27 +28,30 @@ def create_recurring_request_every_day():
         address = client.main_address
 
         if not address:
+            logger.info(f"Schedule # {schedule.pk} doesn't have a address")
             continue
 
         if weekday not in schedule.days:
+            logger.info(f"Schedule # {schedule.pk} doesn't include this weekday {weekday}")
             continue
 
         if schedule.status != settings.ACTIVE:
+            logger.info(f"Schedule # {schedule.pk} not active")
             continue
 
         key = f"recurring_schedule:{schedule.pk}"
         if exists_in_execution_cache(key):
+            logger.info(f"Request for schedule # {schedule.pk} already handled")
             continue
 
         logger.info(f"Start of handling schedule # {schedule.pk}")
 
         service = RequestService(client=client)
-        request, _ = service.get_or_create(
-            extra_query={"schedule": schedule, "address": address},
-            extra_defaults={
-                "comment": schedule.comment,
-                "is_rush": schedule.is_rush,
-            },
+        request = service.create(
+            schedule=schedule,
+            address=address,
+            comment=schedule.comment,
+            is_rush=schedule.is_rush,
         )
 
         add_to_execution_cache(key)
