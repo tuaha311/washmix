30eea2c370f352a9bb1ae5db392905ac91684f02
Muhammad Amsal <amsal134215@gmail.com>
2021-11-05 20:47:16 +0500

    cron delete the users if could'nt signup after given hours

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -1,7 +1,13 @@
 import logging
 
+from django.contrib.auth import get_user_model
+from django.utils.timezone import localtime
+
 import dramatiq
 from periodiq import cron
+
+from settings.base import DELETE_USER_AFTER_TIMEDELTA
+from users.models import Client, Customer
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +20,48 @@ def periodic_scheduler_health():
     """
 
     logger.info("Periodic scheduler health - OK")
+
+
+# every hour
+@dramatiq.actor(periodic=cron("0 * * * *"))
+def archive_not_signedup_users():
+    """
+    Deleting the users that were unable to signup after 6 hours of user creation
+    Moving the user to archived user table in archived app
+    """
+    delete_clients = Client.objects.filter(
+        card_list__isnull=True, created__lt=localtime() - DELETE_USER_AFTER_TIMEDELTA
+    )
+
+    if delete_clients:
+        for client in delete_clients:
+            phone = ""
+            full_name = ""
+            address = ""
+            zip_code = ""
+
+            user = client.user
+            if client.main_phone:
+                phone = client.main_phone.number
+            if client.main_address:
+                address = (
+                    f"{client.main_address.address_line_1} {client.main_address.address_line_2}"
+                )
+                zip_code = client.main_address.zip_code.value
+            if user.first_name:
+                full_name = f"{user.first_name} {user.last_name}"
+
+            Customer.objects.get_or_create(
+                email=user.email,
+                defaults={
+                    "phone": phone,
+                    "full_name": full_name,
+                    "address": address,
+                    "zip_code": zip_code,
+                },
+            )
+
+            user.delete()
 
 
 @dramatiq.actor
