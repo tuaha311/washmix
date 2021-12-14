97ad47014176fb0b939a52b4ca114af990e42d17
Muhammad Amsal <amsal134215@gmail.com>
2021-12-15 00:34:14 +0500

    added Coupon code and discounts in invoice

--- app/notifications/templatetags/full_static.py
+++ app/notifications/templatetags/full_static.py
@@ -12,3 +12,23 @@ def full_static(path):
     host = f"{settings.PROTOCOL}://{settings.DOMAIN}"
 
     return f"{host}{static_url}"
+
+
+@register.simple_tag
+def subtract_value_discount(value, arg):
+    return round(value - arg / 100, 1)
+
+
+@register.simple_tag
+def subtract_percentage_discount(value, arg, amount):
+    return round(value - (amount / 100) * arg, 1)
+
+
+@register.simple_tag
+def get_percentage_discount(value, arg):
+    return round((value / 100) * arg, 1)
+
+
+@register.simple_tag
+def get_value_discount(value, arg):
+    return round(arg / 100, 1)
