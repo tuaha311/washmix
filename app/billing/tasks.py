a5573ae2882bb6f332bbf435b66f279fabe2e6f1
Shoaib <shoaibmoeen4343@gmail.com>
2022-05-24 16:02:58 +0500

    SMS Notification on credit back

--- app/billing/tasks.py
+++ app/billing/tasks.py
@@ -9,7 +9,7 @@ from periodiq import cron
 
 from billing.utils import add_money_to_balance
 from core.utils import add_to_execution_cache, convert_cent_to_dollars, exists_in_execution_cache
-from notifications.tasks import send_email
+from notifications.tasks import send_email, send_sms
 from orders.containers.order import OrderContainer
 from users.models import Client
 
@@ -60,6 +60,22 @@ def accrue_credit_back_every_3_month():
 
         send_email.send(
             event=settings.ACCRUE_CREDIT_BACK,
+            recipient_list=recipient_list,
+            extra_context={
+                "client_id": client_id,
+                "dollar_credit_back": dollar_credit_back,
+                "dollar_balance": dollar_balance,
+                "old_balance": convert_cent_to_dollars(
+                    int(transaction.invoice.order.balance_before_purchase)
+                ),
+                "new_balance": convert_cent_to_dollars(
+                    int(transaction.invoice.order.balance_after_purchase)
+                ),
+            },
+        )
+        recipient_list = [client.main_phone]
+        send_sms.send(
+            event=settings.SMS_CREDIT_BACK,
             recipient_list=recipient_list,
             extra_context={
                 "client_id": client_id,
