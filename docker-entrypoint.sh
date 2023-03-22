94fac6ae007c29c1573ac6128a7dba4c04dd8415
Hamza Khan <Hamzak106@gmail.com>
2023-03-22 11:55:29 -0700

    Updated Entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,31 +4,29 @@ set -e
 
 action=$1
 
-cd app
-
 if [ "$action" = "worker" ]
 then
   echo "Running dramatiq worker"
-  dramatiq --threads 1 --processes 1 -v settings.dramatiq
+  dramatiq --threads 1 --processes 1 -v app/settings.dramatiq
 
 elif [ "$action" = "migrations" ]
 then
   echo "Running migrations"
-  python manage.py migrate --noinput
+  python app/manage.py migrate --noinput
 
 elif [ "$action" = "scheduler" ]
 then
   echo "Running periodiq scheduler"
-  periodiq -v settings.dramatiq
+  periodiq -v app/settings.dramatiq
 
 else
   echo "Running django application"
-  python manage.py collectstatic --noinput
+  python app/manage.py collectstatic --noinput
   ###Added for use in Heroku
   if [ -n "$PORT" ]
   then
-    gunicorn --bind 0.0.0.0:${PORT} --workers 3 settings.wsgi:application
+    gunicorn --bind 0.0.0.0:${PORT} --workers 3 app/settings.wsgi:application
   else
-    gunicorn --bind 0.0.0.0:8000 --workers 3 settings.wsgi:application
+    gunicorn --bind 0.0.0.0:8000 --workers 3 app/settings.wsgi:application
   fi
 fi
