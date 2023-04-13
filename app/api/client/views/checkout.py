3f33ec026569d85ee02085b756a633a8a4dffe4c
Hamza Khan <Hamzak106@gmail.com>
2023-04-13 07:45:42 -0700

    Moved Signup Email from 1st step to after creating the orders

--- app/api/client/views/checkout.py
+++ app/api/client/views/checkout.py
@@ -1,3 +1,7 @@
+import logging
+
+from django.conf import settings
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -8,6 +12,10 @@ from api.client.serializers.checkout import (
 )
 from archived.models import ArchivedCustomer
 from billing.services.checkout import WelcomeService
+from notifications.models import Notification, NotificationTypes
+from notifications.tasks import send_admin_client_information, send_email, send_sms
+
+logger = logging.getLogger(__name__)
 
 
 class WelcomeCheckoutView(GenericAPIView):
@@ -26,20 +34,19 @@ class WelcomeCheckoutView(GenericAPIView):
         client = request.user.client
         welcome_service = WelcomeService(client, request, order)
         address, billing_address = welcome_service.checkout(user, raw_address, raw_billing_address)
-        try:
-            # Deleting User Data if the client was already in our archived Customer
-            archived_customer = ArchivedCustomer.objects.filter(email=client.email).delete()
-            # Confirming that the deletion was successful
-            if archived_customer[0] > 0:
-                print(
-                    f"{archived_customer[0]} records for {client.email} were successfully deleted."
-                )
-            else:
-                print(f"No records found for {client.email}. Nothing was deleted.")
 
-        except Exception as e:
-            print(f"An error occurred while deleting records for {client.email}.")
-            print(f"Error details: {e}")
+        # Deleting User Data if the client was already in our archived Customer
+        archived_customer = ArchivedCustomer.objects.filter(email=client.email).delete()
+        # Confirming that the deletion was successful
+        if archived_customer[0] > 0:
+            logger.info(
+                f"{archived_customer[0]} records for {client.email} were successfully deleted."
+            )
+        else:
+            logger.info(f"No records found for {client.email}. Nothing was deleted.")
+
+        # Send the Welcome email to the user
+        self.send_welcome_email(client)
 
         response_body = {
             "user": user,
@@ -49,3 +56,30 @@ class WelcomeCheckoutView(GenericAPIView):
         response = self.response_serializer_class(response_body).data
 
         return Response(response)
+
+    def send_welcome_email(self, client):
+        main_phone = client.main_phone.number
+        client_id = client.id
+        recipient_list = [client.email]
+
+        send_email.send(
+            event=settings.SIGNUP,
+            recipient_list=recipient_list,
+            extra_context={
+                "client_id": client_id,
+            },
+        )
+
+        send_admin_client_information(client.id, "New User Signed Up")
+        send_sms.send_with_options(
+            kwargs={
+                "event": settings.USER_SIGNUP,
+                "recipient_list": [main_phone],
+                "extra_context": {
+                    "client_id": client.id,
+                },
+            },
+            delay=settings.DRAMATIQ_DELAY_FOR_DELIVERY,
+        )
+
+        Notification.create_notification(client, NotificationTypes.NEW_SIGNUP)
