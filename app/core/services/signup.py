49e36f65fa0b680b52766358f8a2f36868c37744
Hamza Khan <Hamzak106@gmail.com>
2023-04-13 07:45:42 -0700

    Moved Signup Email from 1st step to after creating the orders

--- app/core/services/signup.py
+++ app/core/services/signup.py
@@ -1,11 +1,8 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.transaction import atomic
 
 from billing.services.checkout import WelcomeService
 from billing.stripe_helper import StripeHelper
-from notifications.models import Notification, NotificationTypes
-from notifications.tasks import send_admin_client_information, send_email, send_sms
 from users.models import Client
 
 User = get_user_model()
@@ -15,9 +12,6 @@ class SignupService:
     def signup(self, zipCode, address, email, password, phone) -> Client:
         with atomic():
             client = Client.objects.create_client(email, password, phone)
-            client_id = client.id
-            recipient_list = [client.email]
-
             stripe_helper = StripeHelper(client)
             customer = stripe_helper.customer
 
@@ -27,30 +21,5 @@ class SignupService:
             service = WelcomeService(client, None, None)
             raw_address = {"zip_code": zipCode, "address_line_1": address}
             address = service._create_signup_main_address(raw_address)
-
-        main_phone = client.main_phone.number
-
-        send_email.send(
-            event=settings.SIGNUP,
-            recipient_list=recipient_list,
-            extra_context={
-                "client_id": client_id,
-            },
-        )
-
-        send_admin_client_information(client.id, "New User Signed Up")
-
-        send_sms.send_with_options(
-            kwargs={
-                "event": settings.USER_SIGNUP,
-                "recipient_list": [main_phone],
-                "extra_context": {
-                    "client_id": client.id,
-                },
-            },
-            delay=settings.DRAMATIQ_DELAY_FOR_DELIVERY,
-        )
-
-        Notification.create_notification(client, NotificationTypes.NEW_SIGNUP)
 
         return client
