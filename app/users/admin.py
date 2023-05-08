99d7690577104ae9cfd0c892836665ab01320e0a
Hamza Khan <Hamzak106@gmail.com>
2023-05-08 06:28:06 -0700

    Revert "Merge pull request #85 from WashMix/dev"
    This reverts commit 1a9d81c8289f210f4523cd8f4101e414e3f59d89, reversing
    changes made to ebf33ccf3b8b6fcc563b5ddf63ba28b81a4e48ef.

--- app/users/admin.py
+++ app/users/admin.py
@@ -228,6 +228,7 @@ class RequestInlineAdmin(admin.TabularInline):
     model = Request
     form = RequestInlineForm
     extra = 0
+    can_delete = False
     per_page = LIMIT
 
     def get_formset(self, request, obj=None, **kwargs):
