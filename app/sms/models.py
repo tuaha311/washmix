c3c2685604fb87f0158754a64f375a32897c7e65
Hamza Khan <Hamzak106@gmail.com>
2023-06-16 14:47:20 -0700

    Updated the Template and improved UI

--- app/sms/models.py
+++ app/sms/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class SMSTemplate(models.Model):
+    name = models.CharField(max_length=255, default='Untitled')
     content = models.TextField()
 
     class Meta:
-        verbose_name = "Send SMS"
-        verbose_name_plural = "Send SMS"
+        verbose_name = "SMS Template"
+        verbose_name_plural = "SMS Templates"
 
     def __str__(self) -> str:
-        return self.content[:50]
+        return self.name
