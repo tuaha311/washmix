3e43188d682ecc207bb8f42396829787b4fcca3b
Hamza Khan <Hamzak106@gmail.com>
2023-06-21 10:14:16 -0700

    Updated the implementation and fixed the code.

--- app/sms/views.py
+++ app/sms/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import redirect, render, render_to_response
+from django.shortcuts import redirect, render
 
 from notifications.tasks import send_sms as Send_SMS
 from users.models import Client as clients
@@ -15,62 +15,59 @@ from .models import SMSTemplate
 logger = logging.getLogger(__name__)
 
 
+def get_selected_customers(request):
+    zip_code_param = request.GET.get("zip_code")
+    city_param = request.GET.get("city")
+    address_param = request.GET.get("address")
+    phone_param = request.GET.get("phone")
+
+    selected = ""
+    if zip_code_param:
+        defaults = clients.objects.filter(billing_address__zip_code=zip_code_param)
+        for default in defaults:
+            selected += str(default.id) + ","
+
+    elif city_param:
+        defaults = clients.objects.filter(Q(billing_address__address_line_1__contains=city_param))
+        for default in defaults:
+            selected += str(default.id) + ","
+    elif address_param:
+        defaults = clients.objects.filter(billing_address__address_line_1=address_param)
+        for default in defaults:
+            selected += str(default.id) + ","
+    elif phone_param:
+        default = clients.objects.get(main_phone__number=phone_param)
+        selected += str(default.id)
+    else:
+        selected = None
+
+    return selected
+
+
 def outbound_sms(request):
     if request.method == "GET":
-        zip_code_param = request.GET.get("zip_code")
-        city_param = request.GET.get("city")
-        address_param = request.GET.get("address")
-        phone_param = request.GET.get("phone")
+        selected = get_selected_customers(request)
 
-        selected = ""
-        if zip_code_param:
-            defaults = clients.objects.filter(billing_address__zip_code=zip_code_param)
-            for default in defaults:
-                selected = selected + str(default.id) + ","
+        customers = clients.objects.all()
+        billing_address = [customer.billing_address for customer in customers]
 
-        elif city_param:
-            defaults = clients.objects.filter(
-                Q(billing_address__address_line_1__contains=city_param)
-            )
-            for default in defaults:
-                selected = selected + str(default.id) + ","
-        elif address_param:
-            defaults = clients.objects.filter(billing_address__address_line_1=address_param)
-            for default in defaults:
-                selected = selected + str(default.id) + ","
-        elif phone_param:
-            default = clients.objects.get(main_phone__number=phone_param)
-            selected = selected + str(default.id)
-        else:
-            selected = None
-    customers = clients.objects.all()
-    billing_address = []
-    for customer in customers:
-        billing_address.append(customer.billing_address)
-    zips = []
-    for zip in billing_address:
-        if zip["zip_code"] not in zips:
-            zips.append(zip["zip_code"])
-    addressess = []
-    for address in billing_address:
-        if address["address_line_1"] not in addressess:
-            addressess.append(address["address_line_1"])
-    cities = []
-    for city in addressess:
-        address_components = city.split(",")
-        city = address_components[-1].strip()
-        if city not in cities:
-            cities.append(city)
+        zips = list(set(address["zip_code"] for address in billing_address))
+        addresses = list(set(address["address_line_1"] for address in billing_address))
+        cities = [
+            address.split(",")[-1].strip()
+            for address in addresses
+            if address.split(",")[-1].strip()
+        ]
 
-    templates = SMSTemplate.objects.all()
-    context = {
-        "customers": customers,
-        "templates": templates,
-        "selected": selected,
-        "zips": zips,
-        "cities": cities,
-    }
-    return render(request, "outbound_sms.html", context)
+        templates = SMSTemplate.objects.all()
+        context = {
+            "customers": customers,
+            "templates": templates,
+            "selected": selected,
+            "zips": zips,
+            "cities": cities,
+        }
+        return render(request, "outbound_sms.html", context)
 
 
 def send_sms(request):
@@ -78,14 +75,20 @@ def send_sms(request):
         customer_ids_string = request.POST.getlist("customers")
         template_id = request.POST.get("template")
 
-        customer_ids = json.loads(customer_ids_string[0])
+        if not customer_ids_string or not template_id:
+            return HttpResponse("Invalid customer IDs or template ID.")
 
-        customer_ids = [int(id) for id in customer_ids]
-        # return HttpResponse()
         try:
             template = SMSTemplate.objects.get(pk=template_id)
         except SMSTemplate.DoesNotExist:
             return HttpResponse("Selected template does not exist.")
+
+        customer_ids = json.loads(customer_ids_string[0])
+
+        try:
+            customer_ids = [int(id) for id in customer_ids]
+        except ValueError:
+            return HttpResponse("Invalid customer IDs.")
 
         customers = clients.objects.filter(pk__in=customer_ids)
 
@@ -93,7 +96,6 @@ def send_sms(request):
             # Prepare recipient list and context
             recipient_list = [customer.main_phone.number]
             event = settings.PROMOTION
-            template = SMSTemplate.objects.get(pk=template_id)
             context = {
                 "template": template.content,
             }
