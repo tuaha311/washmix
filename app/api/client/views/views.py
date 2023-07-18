cdb933d46b6ede8d1755ab03df9f19af6515999b
Hamza Khan <hamzak106@gmail.com>
2023-07-18 12:01:08 -0700

    Added ability to select for the week, month, or year

--- app/api/client/views/views.py
+++ app/api/client/views/views.py
@@ -12,9 +12,26 @@ from orders.models import Order
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from users.models import Client
+from datetime import datetime, timedelta
 
 
-def generate_client_pdf_core(request, client_id):
+def generate_client_pdf_core(request, client_id, duration=None):
+        start_date = datetime.now().date()
+        
+        if duration == 'week':
+            start_date -= timedelta(days=7)
+            pdf_filename = f"week_{client_id}.pdf"
+        elif duration == 'month':
+            start_date -= timedelta(days=30)
+            pdf_filename = f"month_{client_id}.pdf"
+        elif duration == 'year':
+            start_date -= timedelta(days=365)
+            pdf_filename = f"year_{client_id}.pdf"
+        else:
+            start_date = "2000-01-01"
+            pdf_filename = f"{client_id}.pdf"
+        
+        print(start_date)
         template = "client_report.html"
         media_root = Base.MEDIA_ROOT
         client_directory = os.path.join(media_root, "clients")
@@ -23,7 +40,10 @@ def generate_client_pdf_core(request, client_id):
 
         purpose = "credit"  # Purpose set to 'Credit by WashMix'
         
-        invoice_list = client.invoice_list.filter(purpose=purpose).annotate(
+        invoice_list = client.invoice_list.filter(
+        purpose=purpose,
+        created__gte=start_date
+        ).annotate(
             balance=Subquery(
                 client.transaction_list.filter(invoice=OuterRef('pk'))
                 .values('invoice')
@@ -32,7 +52,7 @@ def generate_client_pdf_core(request, client_id):
             )
         )
         context = {"client": client, "invoice_list": invoice_list, "is_pdf": True}
-        client_orders = Order.objects.filter(client=client)
+        client_orders = Order.objects.filter(client=client, created__gte=start_date)
         context["client_orders"] = client_orders
 
         printable_page = render_to_string(template, context)
@@ -44,7 +64,6 @@ def generate_client_pdf_core(request, client_id):
             HTML(string=printable_page).write_pdf(temp_pdf)
 
         client_id = client.id
-        pdf_filename = f"{client_id}.pdf"
         pdf_path = os.path.join(client_directory, pdf_filename)
 
         shutil.move(temp_pdf.name, pdf_path)
@@ -61,8 +80,9 @@ def generate_client_pdf_core(request, client_id):
 def generate_client_pdf(request):
         if request.method == "POST":
             client_id = request.POST.get("client_id")
+            duration = request.POST.get("duration")
             if client_id:
-                return HttpResponse(generate_client_pdf_core(request, client_id))
+                return HttpResponse(generate_client_pdf_core(request, client_id, duration))
             else:
                 raise HTTPException("Invalid payload")
 
