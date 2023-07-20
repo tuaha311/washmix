90ac4218f139ba3098629573f0c3bd451165f60f
Hamza Khan <hamzak106@gmail.com>
2023-07-20 11:46:33 -0700

    Added a separate template for client's pdf

--- app/api/client/views/views.py
+++ app/api/client/views/views.py
@@ -12,6 +12,9 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from users.models import Client
 from datetime import datetime, timedelta
+from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
 
 
 def generate_client_pdf_core(request, client_id, duration=None, start_date=None, end_date=None):
@@ -26,7 +29,6 @@ def generate_client_pdf_core(request, client_id, duration=None, start_date=None,
 
     context = {
         "client": client,
-        
         "invoice_list": invoice_list,
         "is_pdf": True,
         "client_orders": client_orders,
@@ -39,30 +41,32 @@ def generate_client_pdf_core(request, client_id, duration=None, start_date=None,
 
     with tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as temp_pdf:
         HTML(string=printable_page).write_pdf(temp_pdf)
-
+    print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+    print(client_id, "client_id")
+    print(duration, "duration")
+    print(start_date, "start_date")
+    print(end_date, "end_date")
+    print(pdf_filename, "pdf_filename")
     media_root = Base.MEDIA_ROOT
     client_directory = os.path.join(media_root, "clients")
     os.makedirs(client_directory, exist_ok=True)
+    pdf_directory = os.path.join(Base.MEDIA_URL, "clients")
     pdf_path = os.path.join(client_directory, pdf_filename)
 
     shutil.move(temp_pdf.name, pdf_path)
     os.remove(temp_html.name)
-
+    pdf_path = os.path.join(pdf_directory, pdf_filename)
     success_message = "PDF generated successfully"
     response_data = {"pdf_path": pdf_path, "message": success_message}
     return JsonResponse(response_data)
 
 
 def calculate_start_date_and_filename(client_id, duration, start_date):
-    print("")
-    print("")
-    print("")
-    print("")
-    print("start_date", start_date)
-    print("duration", duration)
-    print("client_id", client_id)
     if not start_date:
         start_date = datetime.now().date()
+    else:
+        pdf_filename = f"custom_{client_id}.pdf"
+        return start_date, pdf_filename
 
     if duration == "week":
         start_date -= timedelta(days=7)
@@ -74,7 +78,7 @@ def calculate_start_date_and_filename(client_id, duration, start_date):
         start_date -= timedelta(days=365)
         pdf_filename = f"year_{client_id}.pdf"
     else:
-        start_date = start_date
+        start_date = "2000-01-01"
         pdf_filename = f"{client_id}.pdf"
 
     return start_date, pdf_filename
@@ -84,10 +88,9 @@ def get_filtered_data(client, start_date, end_date=None):
     purpose = "credit"  # Purpose set to 'Credit by WashMix'
 
     if end_date:
-        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        print(start_date)
-        print(end_date)
-        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("-------------------HERRE------------------")
+        print("-------------------start_date------------------", start_date)
+        print("-------------------end_date------------------", end_date)
         invoice_list = client.invoice_list.filter(
             purpose=purpose,
             created__gte=start_date,
@@ -104,6 +107,8 @@ def get_filtered_data(client, start_date, end_date=None):
             client=client, created__gte=start_date, created__lte=end_date
         )
     else:
+        print("-------------------STILLLL THERRE------------------")
+        
         invoice_list = client.invoice_list.filter(
             purpose=purpose,
             created__gte=start_date,
@@ -130,11 +135,59 @@ def generate_client_pdf(request):
         if client_id:
             return HttpResponse(
                 generate_client_pdf_core(
-                    request, client_id=client_id, duration=duration, start_date=start_date, end_date=end_date
-                )
+                    request,
+                    client_id=client_id,
+                    duration=duration,
+                    start_date=start_date,
+                    end_date=end_date,
+                ), messages.success(request, "Client's PDF generated successfully.")
             )
         else:
+            messages.error(request, "Client's PDF could not be generated.")
             raise HTTPException("Invalid payload")
+        
+    if request.method == "GET":
+        client_id = request.GET.get("client_id")
+        week_pdf_name = f"week_{client_id}.pdf"
+        week_pdf_path = os.path.join(settings.MEDIA_URL, "clients", week_pdf_name)
+        full_path = os.path.join(settings.MEDIA_ROOT, "clients", week_pdf_name)
+        if os.path.exists(full_path):
+            week_pdf_path = week_pdf_path
+        else:
+            week_pdf_path = "-"
+
+        month_pdf_name = f"month_{client_id}.pdf"
+        month_pdf_path = os.path.join(settings.MEDIA_URL, "clients", month_pdf_name)
+        full_path = os.path.join(settings.MEDIA_ROOT, "clients", month_pdf_name)
+        if os.path.exists(full_path):
+            month_pdf_path = month_pdf_path
+        else:
+            month_pdf_path = "-"
+
+        year_pdf_name = f"year_{client_id}.pdf"
+        year_pdf_path = os.path.join(settings.MEDIA_URL, "clients", year_pdf_name)
+        full_path = os.path.join(settings.MEDIA_ROOT, "clients", year_pdf_name)
+        if os.path.exists(full_path):
+            year_pdf_path = year_pdf_path
+        else:
+            year_pdf_path = "-"
+            
+        all_times_pdf_name = f"{client_id}.pdf"
+        all_times_pdf_path = os.path.join(settings.MEDIA_URL, "clients", all_times_pdf_name)
+        full_path = os.path.join(settings.MEDIA_ROOT, "clients", all_times_pdf_name)
+        if os.path.exists(full_path):
+            all_times_pdf_path = all_times_pdf_path
+        else:
+            all_times_pdf_path = "-"
+
+        context = {
+            "client_id": client_id,
+            "week_pdf_path": week_pdf_path,
+            "month_pdf_path": month_pdf_path,
+            "year_pdf_path": year_pdf_path,
+            "all_times_pdf_path": all_times_pdf_path,
+        }
+        return render(request, "generate_client_pdf.html", context)
 
 
 @csrf_exempt
--- app/users/admin.py
+++ app/users/admin.py
@@ -367,10 +367,6 @@ class ClientAdmin(AdminUpdateFieldsMixin, AdminWithSearch):
         "stripe_id",
         "full_address",
         "address_line_2",
-        "one_week_pdf",
-        "one_month_pdf",
-        "one_year_pdf",
-        "timeframe_pdf",
         "pdf_path",
     ]
     form = ClientForm
@@ -412,56 +408,12 @@ class ClientAdmin(AdminUpdateFieldsMixin, AdminWithSearch):
     get_main_phone_number.short_description = "Main Phone Number"  # Set column header in admin
 
     actions = ["full_delete_action", "generate_client_pdf"]
-    
-    def one_week_pdf(self, client):
-        pdf_filename = f"week_{client.id}.pdf"
-        pdf_path = os.path.join(settings.MEDIA_URL, "clients", pdf_filename)
-        widget_html = (
-                render_to_string("widgets/href.html", context={"pdf_path": pdf_path})) + render_to_string("widgets/last_week_pdf.html", context={"client_id": client.id, "duration": "week"})
-
-        return mark_safe(widget_html.replace('\n', ''))
-    
-    def one_month_pdf(self, client):
-        pdf_filename = f"moth_{client.id}.pdf"
-        pdf_path = os.path.join(settings.MEDIA_URL, "clients", pdf_filename)
-        widget_html = (
-                render_to_string("widgets/href.html", context={"pdf_path": pdf_path})) + render_to_string("widgets/last_month_pdf.html", context={"client_id": client.id, "duration": "month"})
-
-        return mark_safe(widget_html.replace('\n', ''))
-    
-    def one_year_pdf(self, client):
-        pdf_filename = f"year_{client.id}.pdf"
-        pdf_path = os.path.join(settings.MEDIA_URL, "clients", pdf_filename)
-        widget_html = (
-                render_to_string("widgets/href.html", context={"pdf_path": pdf_path})) + render_to_string("widgets/last_year_pdf.html", context={"client_id": client.id, "duration": "year"})
-
-        return mark_safe(widget_html.replace('\n', ''))
-    
-    def timeframe_pdf(self, client):
-        pdf_filename = f"year_{client.id}.pdf"
-        pdf_path = os.path.join(settings.MEDIA_URL, "clients", pdf_filename)
-        widget_html = (
-                render_to_string("widgets/href.html", context={"pdf_path": pdf_path})) + render_to_string("widgets/client_pdf_timeframe.html", context={"client_id": client.id, "duration": "year"})
-
-        return mark_safe(widget_html.replace('\n', ''))
-    
-    def pdf_by_duration(self, client, duration):
-        pdf_filename = f"{duration}_{client.id}.pdf"
-        pdf_path = os.path.join(settings.MEDIA_URL, "clients", pdf_filename)
-        widget_html = (
-                render_to_string("widgets/href.html", context={"pdf_path": pdf_path})) + render_to_string("widgets/client_pdf.html", context={"client_id": client.id, "duration": duration})
-
-        return mark_safe(widget_html.replace('\n', ''))
 
     def pdf_path(self, client):
-        pdf_filename = f"{client.id}.pdf"
-        pdf_path = os.path.join(settings.MEDIA_URL, "clients", pdf_filename)
-        if pdf_path:
-            context = {"pdf_path": pdf_path}
-            widget = render_to_string("widgets/href.html", context=context)
-            return mark_safe(widget)
-
-        return "-"
+        path = f"/api/client/generate-pdf?client_id={client.id}"
+        context = {"pdf_path": path}
+        widget = render_to_string("widgets/href.html", context=context)
+        return mark_safe(widget)
 
     def generate_client_pdf(modeladmin, request, queryset):
         for client in queryset:
