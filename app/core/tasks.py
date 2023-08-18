28ac6ff4798dac4550d9bc6511bda7cfc449df02
Hamza Khan <hamzak106@gmail.com>
2023-08-18 05:21:52 -0700

    Removed redundant code

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -252,44 +252,6 @@ def delete_archived_customers_who_signed_up_already():
         client.delete()
 
 
-# every 10 minutes
-@dramatiq.actor(periodic=cron("*/10 * * * *"))
-def archive_periodic_promotional_emails():
-    email_customers = ArchivedCustomer.objects.filter(
-        promo_email_sent_count__lt=settings.TOTAL_PROMOTIONAL_EMAIL_COUNT
-    )
-    current_time = localtime()
-    for client in email_customers:
-        email_time = client.promo_email_send_time
-        sent_count = client.promo_email_sent_count
-        # Sending Email
-        if (
-            not sent_count
-            and (
-                email_time.strftime("%Y-%m-%d %H:00:00")
-                == current_time.strftime("%Y-%m-%d %H:00:00")
-            )
-        ) or (sent_count and (email_time.date() == current_time.date())):
-            if not client.promo_email_sent_count:
-                email_to_send = settings.FIRST_PROMOTION_EMAIL_ARCHIVE_CUSTOMER
-            elif client.promo_email_sent_count == 1:
-                email_to_send = settings.SECOND_PROMOTION_EMAIL_ARCHIVE_CUSTOMER
-            else:
-                email_to_send = settings.THIRD_PROMOTION_EMAIL_ARCHIVE_CUSTOMER
-            send_email(
-                event=email_to_send,
-                recipient_list=[client.email],
-                extra_context={
-                    "date": datetime.datetime.now().strftime("%d-%B-%Y"),
-                },
-            )
-            client.increase_promo_email_sent_count()
-            time_to_add = get_time_delta_for_promotional_emails(
-                settings.PROMO_EMAIL_PERIODS, client.promo_email_sent_count
-            )
-            client.set_next_promo_email_send_date(time_to_add)
-            client.save()
-
 def derive_required_months(month):
     all_months = []
     for i in range(0, 5):
