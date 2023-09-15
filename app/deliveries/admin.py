c95599780abb9e26d00715956fab5777305c8d9e
Hamza Khan <hamzak106@gmail.com>
2023-09-15 23:03:34 +0500

    Limitized choices for dropoff kind of deliveries

--- app/deliveries/admin.py
+++ app/deliveries/admin.py
@@ -26,7 +26,26 @@ class DeliveryForm(forms.ModelForm):
                 model._meta.get_field("employee").remote_field, "Select an Employee", admin.site
             )
         }
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
+        if instance and instance.kind == DeliveryKind.DROPOFF:
+            self.fields['status'].choices = [
+                (DeliveryStatus.ACCEPTED, DeliveryStatus.MAP[DeliveryStatus.ACCEPTED]),
+                (DeliveryStatus.IN_PROGRESS, DeliveryStatus.MAP[DeliveryStatus.IN_PROGRESS]),
+                (DeliveryStatus.COMPLETED, DeliveryStatus.MAP[DeliveryStatus.COMPLETED]),
+            ]
+            
+class ArchivedDeliveryForm(forms.ModelForm):
+    class Meta:
+        model = Delivery
+        fields = "__all__"
+        widgets = {
+            "employee": CustomAutocompleteSelect(
+                model._meta.get_field("employee").remote_field, "Select an Employee", admin.site
+            )
+        }
 
 class DeliveryInlineAdmin(admin.TabularInline):
     model = Delivery
@@ -70,7 +89,7 @@ class RequestAdmin(AdminWithSearch):
         return queryset, use_distinct
 
 
-class DeliveryAdmin(AdminWithSearch):
+class DeliveryAdminMain(AdminWithSearch):
     change_list_template = 'assets/change_employee.html'
     readonly_fields = [
         "order",
@@ -196,6 +215,19 @@ class DeliveryAdmin(AdminWithSearch):
 
         return super().save_model(request, obj, form, change)
 
+class DeliveryAdmin(DeliveryAdminMain):
+    form = DeliveryForm
+    # autocomplete_fields = ('employee',)
+    def get_changelist_form(self, request, **kwargs):
+        return DeliveryForm
+    
+    
+class ArchivedDeliveryAdmin(DeliveryAdminMain):
+    form = ArchivedDeliveryForm
+    # autocomplete_fields = ('employee',)
+    def get_changelist_form(self, request, **kwargs):
+        return ArchivedDeliveryForm
+    
 
 class ScheduleForm(forms.ModelForm):
     # we are overriding default `days` field widget
