26fd2b79b13419c45aaba8868f669a07efa6150b
Hamza Khan <hamzak106@gmail.com>
2023-10-03 19:21:36 +0500

    Removed unused code

--- app/sms/urls.py
+++ app/sms/urls.py
@@ -1,25 +1,8 @@
 from django.contrib.admin import AdminSite
 from django.urls import path
 
-from .views import outbound_sms, send_sms
+from .views import send_sms
 
-class ExtraAdminURL(AdminSite):
-    def get_urls(self):
-        urls = super().get_urls()
-
-        # Add the custom URL patterns for the admin site
-        urls += [
-            path("sms/outbound-sms/", self.admin_view(outbound_sms), name="outbound_sms"),
-        ]
-
-        return urls
-
-# Register your custom admin site
-my_admin_site = ExtraAdminURL(name="admin-url")
-
-# Define your app-specific URLs
 urlpatterns = [
-    path("admin/", my_admin_site.urls),  # Include your custom admin URLs
     path("send-sms/", send_sms, name="send_sms"),
-    # Add other app-specific URLs here
 ]
