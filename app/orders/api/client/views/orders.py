65825f45d67d0aaac95c3457cd50048cb490a60d
Hamza Khan <hamzak106@gmail.com>
2023-10-11 21:18:19 +0500

    Enhance OrderRetrieveView to Check for Existing PDF Reports
    In the OrderRetrieveView of the client-side API views for orders, we have improved the functionality to generate and retrieve PDF reports for orders. This enhancement includes a check to determine if an existing PDF report for the order already exists before generating a new one.
    
    Changes Made:
    - Imported the 'os' module to facilitate file system operations.
    - Utilized the 'generate_pdf_report_path' function from the 'core.utils' module to obtain the path to an existing PDF report, if one exists.
    - Implemented a conditional check to verify the existence of an existing PDF report. If an existing PDF report is found, it is returned immediately.
    - If an existing PDF report is not found, the code proceeds to generate a new PDF report as before.
    
    This enhancement ensures efficiency by reducing unnecessary PDF generation and enhances the performance of the OrderRetrieveView.

--- app/orders/api/client/views/orders.py
+++ app/orders/api/client/views/orders.py
@@ -1,6 +1,8 @@
+import os
 from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from core.utils import generate_pdf_report_path
 
 from orders.api.pos.serializers.orders import OrderSerializer
 from orders.choices import OrderPaymentChoices
@@ -47,6 +49,13 @@ class OrderRetrieveView(GenericAPIView):
         order_id = self.kwargs.get('pk')  # Use self.kwargs to get the order's primary key
         base_dir = settings.BASE_DIR
         base_url = request.get_host()
+        existing_pdf = generate_pdf_report_path(order_id)
+
+        if os.path.exists(existing_pdf):
+            relative_to_base_dir = existing_pdf.relative_to(base_dir)
+            complete_path = f"https://{base_url}/{str(relative_to_base_dir)}"
+            return str(complete_path)
+
         absolute_path = generate_pdf_from_html(order_id)  # Use the order_id as an argument
 
         relative_to_base_dir = absolute_path.relative_to(base_dir)
