59522f5753a0cf01534da28fb30ba9c293292ff3
Hamza Khan <hamzak106@gmail.com>
2023-10-16 20:11:52 +0500

    Using AND logic for filtering customers

--- app/sms/admin.py
+++ app/sms/admin.py
@@ -19,19 +19,22 @@ class SendSMSAdmin(ClientAdmin):
     actions = ["send_sms"]
     
     def get_params(self, request):
+        template_param = request.POST.get("template")
         zip_code_param = request.POST.get("zip_code")
         city_param = request.POST.get("city")
-        address_param = request.POST.get("address")
+        address_param = request.POST.get("address",{})
         phone_param = request.POST.get("phone")
         selected_customers_json = request.POST.get("selected_customers")  # Get the JSON string
         selected_customers = json.loads(selected_customers_json) if selected_customers_json else []
         selected_customers = list(set(selected_customers))
         data = {}
         selected_customers_ids = [int(id) for id in selected_customers if id.isdigit()]
-
         # Define the base queryset
-        queryset = Client.objects.all()
         
+        if template_param:
+            template_param = SMSTemplate.objects.get(pk=template_param)
+        
+        queryset = Client.objects.all()
         filters = []
         if zip_code_param:
             filters.append(Q(billing_address__zip_code=zip_code_param))
@@ -41,18 +44,19 @@ class SendSMSAdmin(ClientAdmin):
             filters.append(Q(billing_address__address_line_1=address_param))
         if phone_param:
             filters.append(Q(main_phone__number=phone_param))
-        if selected_customers:
-            filters.append(Q(id__in=selected_customers_ids))
 
         # Combine Q objects using OR operator (|) to create a dynamic query
         if filters:
             query = filters.pop()
             for q in filters:
-                query |= q
+                query &= q
+            if selected_customers:
+                query |= Q(id__in=selected_customers_ids)
             queryset = queryset.filter(query)
 
         data = {
             "queryset": queryset,
+            "selected_template": template_param,
             "selected_zip_code": zip_code_param,
             "selected_city": city_param,
             "selected_address": address_param,
