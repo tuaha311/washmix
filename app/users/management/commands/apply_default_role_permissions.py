964af7fcff00fb3754c5a3a809de6dfac16a8718
Hamza Khan <hamzak106@gmail.com>
2023-11-10 21:34:17 +0500

    Added checks and exception in one time query

--- app/users/management/commands/apply_default_role_permissions.py
+++ app/users/management/commands/apply_default_role_permissions.py
@@ -11,23 +11,27 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Find all users with roles that don't have the default
         # users_without_default_role = User.objects.filter(role__isnull=True)
-        users_without_default_role = User.objects.all()
+        users_without_default_role = User.objects.filter(role__isnull=True)
         
         # Apply the default role to these users
         for user in users_without_default_role:
             groups = user.groups.all()
             user_is_admin = any(group.name.lower() in ["admin", "admins"] for group in groups)
+            existing_role = Role.objects.filter(user=user).first()
 
-            # print(user_is_admin)
-            if user.is_superuser:
-                # Superuser should not be assigned the default role
-                Role.objects.create(user=user, position=RoleChoices.SUPERADMIN)
-            if user_is_admin:
-                # Users in the "Admins" group should not be assigned the default role
-                Role.objects.create(user=user, position=RoleChoices.ADMIN)
+            if not existing_role:
+                if user.is_superuser:
+                    # Superuser should not be assigned the default role
+                    Role.objects.create(user=user, position=RoleChoices.SUPERADMIN)
+                elif user_is_admin:
+                    # Users in the "Admins" group should not be assigned the default role
+                    Role.objects.create(user=user, position=RoleChoices.ADMIN)
+                else:
+                    # Check if the user doesn't already have a role
+                    Role.objects.create(user=user, position=RoleChoices.USER)
             else:
-                # Check if the user doesn't already have a role
-                Role.objects.create(user=user, position=RoleChoices.USER)
-                pass
+                # Handle the case where a role already exists for the user
+                # You can update the existing role or skip the user
+                print(f"Role already exists for user {user.username}")
             
         self.stdout.write(self.style.SUCCESS('Default role permissions applied to existing users.'))
